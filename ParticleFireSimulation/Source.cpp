dfe5c0e91921d06c2ca9f17900eb6b84060918f2
Scammell, John <john.scammell@capgemini.com>
2018-04-11 18:52:48 +0100

    Added SDL header and library files, updates to the main()

--- ParticleFireSimulation/Source.cpp
+++ ParticleFireSimulation/Source.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <SDL.h>
+
 using namespace std;
 
-int main(int argc, char * argv[])
+int main(int argc, char **argv)
 {
 	const int SCREEN_WIDTH = 800;
 	const int SCREEN_HEIGHT = 600;
@@ -11,16 +12,69 @@ int main(int argc, char * argv[])
 		cout << "SDL initialization failed. SDL Error: " << SDL_GetError() << endl;
 		return 1;
 	}
-	
-	SDL_Window *window = SDL_CreateWindow("Particle Fire Simulation", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+	SDL_Window *window = SDL_CreateWindow(
+		"Particle Fire Simulation",
+		SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED,
+		SCREEN_WIDTH, SCREEN_HEIGHT,
+		SDL_WINDOW_SHOWN);
 
 	if (window == NULL) {
 		SDL_Quit();
 		return 2;
 	}
 
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+	if (renderer == NULL) {
+		cout << "Could not create renderer" << endl;
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return 3;
+	}
+
+	SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HEIGHT);
+	if (texture == NULL) {
+		cout << "Could not create texture" << endl;
+		SDL_DestroyRenderer(renderer);
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return 4;
+	}
+
+	Uint32 *buffer = new Uint32[SCREEN_WIDTH * SCREEN_HEIGHT];
+
+	memset(buffer, 0xFF, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint32));
+
+	SDL_UpdateTexture(texture, NULL, buffer, SCREEN_WIDTH * sizeof(Uint32));
+	SDL_RenderClear(renderer);
+	SDL_RenderCopy(renderer, texture, NULL, NULL);
+	SDL_RenderPresent(renderer);
+
+	bool quit = false;
+	SDL_Event event;
+	while (!quit) {
+		// Check for messages/events
+
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) {
+				quit = true;
+			}
+		}
+
+		// Update particles
 
 
+
+		// Draw particles
+
+
+
+	}
+
+	delete[] buffer;
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyTexture(texture);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 	return 0;
